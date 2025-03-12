83c75d16d02f93d609e6b1ab57c28037b5d3a4af
Ahmeddzz <81853174+Ahmeddzz@users.noreply.github.com>
2025-03-12 13:30:03 +0100

    Add Flyway and Dockerfile

--- src/main/java/com/ahmedzahran/catelog_service/domain/Book.java
+++ src/main/java/com/ahmedzahran/catelog_service/domain/Book.java
@@ -25,6 +25,7 @@ public record Book(
         @NotNull(message = "The book price must be defined.")
         @Positive(message = "The book price must be positive.")
         Double price,
+        String publisher,
         @CreatedDate
         Instant createdDate,
 
@@ -32,7 +33,7 @@ public record Book(
         Instant lastModifiedDate,
         @Version int version
 ) {
-        public static Book of(String isbn, String title, String author, Double price) {
-                return new Book(null, isbn, title, author, price, null, null, 0);
+        public static Book of(String isbn, String title, String author, Double price, String publisher) {
+                return new Book(null, isbn, title, author, price, publisher, null, null, 0);
         }
 }
